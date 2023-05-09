7a7d5ecc80509a394bc6344b26de579966949525
SeyyedAliTabatabaei <tabatabaeiali7@gmail.com>
2023-05-09 23:23:22 +0330

    Initial Commit

---
+++ My Tasks/My_TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_TasksApp.swift
+//  My Tasks
+//
+//  Created by Seyyed Ali Tabatabaei on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct My_TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ My TasksUITests/My_TasksUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  My_TasksUITestsLaunchTests.swift
+//  My TasksUITests
+//
+//  Created by Seyyed Ali Tabatabaei on 5/9/23.
+//
+
+import XCTest
+
+final class My_TasksUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
